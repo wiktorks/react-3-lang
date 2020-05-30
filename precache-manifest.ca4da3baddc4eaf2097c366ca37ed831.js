184f66b05d614be13a754c5d4999cf9fd891a680
WiktorKS <wiktorks1994@gmail.com>
2020-05-30 03:20:16 +0200

    Updates

---
+++ precache-manifest.ca4da3baddc4eaf2097c366ca37ed831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0454a219daf62770cf4582215fe672f9",
+    "url": "/react-3-lang/index.html"
+  },
+  {
+    "revision": "292a3fe10197510895be",
+    "url": "/react-3-lang/static/css/main.8023910a.chunk.css"
+  },
+  {
+    "revision": "1c504c1396bc99150dd9",
+    "url": "/react-3-lang/static/js/2.edfc5a1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-3-lang/static/js/2.edfc5a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292a3fe10197510895be",
+    "url": "/react-3-lang/static/js/main.0d47bda9.chunk.js"
+  },
+  {
+    "revision": "cee92c786a48bed117a6",
+    "url": "/react-3-lang/static/js/runtime-main.4a85d2e6.js"
+  },
+  {
+    "revision": "afbac90db70ac852d92e069ec09e19ae",
+    "url": "/react-3-lang/static/media/england.afbac90d.png"
+  }
+]);
