24a41cd89fb766db0183ed198625cac553be8fd8
WiktorKS <wiktorks1994@gmail.com>
2020-05-30 03:47:19 +0200

    Test update

--- src/components/LangChanger/LangChanger.test.js
+++ src/components/LangChanger/LangChanger.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { act } from 'react-dom/test-utils';
 
 import LangChanger from './LangChanger';
 import styles from './LangChanger.module.css';
-import { langs, LangContext } from '../../lib/languages';
 
 configure({ adapter: new Adapter() });
 
 describe('<LangChanger />', () => {
-    const wrapper = mount(<LangChanger />);
+    let wrapper;
+    act(() => {
+        wrapper = mount(<LangChanger />);
+    });
     const polishImg = wrapper.find('[alt="polska"]');
     const englishImg = wrapper.find('[alt="anglia"]');
     // beforeEach()
@@ -20,12 +23,5 @@ describe('<LangChanger />', () => {
     it('Has polish language as default', () => {
         expect(polishImg.hasClass(styles.selected)).toBeTruthy();
         expect(englishImg.hasClass(styles.selected)).toBeFalsy();
-    });
-
-    it('Switches between languages correctly', () => {
-        englishImg.prop('onClick')();
-        // englishImg.update();
-        expect(polishImg.hasClass(styles.selected)).toBeFalsy();
-        expect(englishImg.hasClass(styles.selected)).toBeTruthy();
     });
 });
